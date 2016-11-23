05b12a5da9dd00339df8850c83d97e32d94855dd
Nitin Pasumarthy <npasumarthy@apple.com>
2016-11-23 12:08:29 -0800

    Added exception handling

--- content-script.js
+++ content-script.js
@@ -1,37 +1,43 @@
 console.log('FF: Loaded content script');
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	let form = request.data;
-	let fieldsNotFound = []
-	$('label').each(function() {
-		let labelText = $(this).text().trim();
-		let fieldId = $(this).attr('for');
-		
-		if(form[labelText]) {
-			let fieldRef = $(`#${fieldId}`);
-			let fieldType = fieldRef.prop("tagName");
-			fieldType = fieldType === "INPUT" ? fieldRef.attr('type') : fieldType;
+	try {
+		let form = request.data;
+		let fieldsNotFound = []
+		$('label').each(function() {
+			let labelText = $(this).text().trim();
+			let fieldId = $(this).attr('for');
+			
+			if(form[labelText]) {
+				let fieldRef = $(`#${fieldId}`);
+				let fieldType = fieldRef.prop("tagName");
+				fieldType = fieldType === "INPUT" ? fieldRef.attr('type') : fieldType;
 
-			switch(fieldType) {
-				case "checkbox":
-				case "CHECKBOX": 	fieldRef.attr({"checked": true});
-									break;
+				switch(fieldType) {
+					case "checkbox":
+					case "CHECKBOX": 	fieldRef.attr({"checked": true});
+										break;
 
-				case "SELECT": 		fieldRef.children("option").each(function() { 
-										if($(this).text().trim() === form[labelText]) { 
-											$(this).prop("selected", true) 
-										} 
-									});
-									break;
+					case "SELECT": 		fieldRef.children("option").each(function() { 
+											if($(this).text().trim() === form[labelText]) { 
+												$(this).prop("selected", true) 
+											} 
+										});
+										break;
 
-				default: 			fieldRef.val(form[labelText]);
-									break;
+					default: 			fieldRef.val(form[labelText]);
+										break;
+				}
 			}
-		}
-		else {
-			fieldsNotFound.push(labelText);
-		}
-	});
-	console.log(`FF: Fields not filled - ${fieldsNotFound.join(",")}`)
-	// sendResponse({response: "Got it"});
+			else {
+				fieldsNotFound.push(labelText);
+			}
+		});
+		console.log(`FF: Fields not filled - ${fieldsNotFound.join(",")}`)
+		sendResponse({response: "Sucess"});
+	}
+	catch(err) {
+		console.error(err);
+		sendResponse({response: err});
+	}
 });
--- popup.js
+++ popup.js
@@ -1,16 +1,21 @@
 $(function() {
 	$("#fill").click(function() {
-		let data = JSON.parse($("#form_data").val());
-		let defaults = {
-			"Software Engineering": true,
-			"United States": true
-		}
+		try {
+			let data = JSON.parse($("#form_data").val());
+			let defaults = {
+				"Software Engineering": true,
+				"United States": true
+			}
 
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		    let mergedData = Object.assign({}, defaults, data);
-			chrome.tabs.sendMessage(tabs[0].id, {"data": mergedData}, function(response) {
-			  console.log(response);
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			    let mergedData = Object.assign({}, defaults, data);
+				chrome.tabs.sendMessage(tabs[0].id, {"data": mergedData}, function(response) {
+				  console.log(response);
+				});
 			});
-		});
+		}
+		catch(err) {
+			console.error(err);
+		}
 	});
 });
